8e9d7da3ea1610035e1cc2d4be0d5cc8490e5ab2
Arne <11283066+dulouie@users.noreply.github.com>
2023-07-25 18:58:56 +0200

    save funktioniert

--- docs/dist/iu-kennlinie.js
+++ docs/dist/iu-kennlinie.js
@@ -1,53 +1,57 @@
-let img;
-let paint = false;
-let mpp = false;
+let sketch = function(p) {
+  let img;
+  let paint = false;
+  let mpp = false;
 
-function preload() {
-  img = loadImage('dist/pictures/iu-kennlinie.png');
-}
-
-function setup() {
-  let cnv = createCanvas(600, 400);
-  cnv.parent('iukenn1');
-}
-
-function draw() {
-  let myDegrees = map(mouseY, 0, height, 250, 400);
-  let v = p5.Vector.fromAngle(radians(myDegrees), 500);
-  let vx = v.x;
-  let vy = v.y;
-
-  background(255)
-  image(img,0,0)
-  strokeWeight(3)
-  stroke(255,0,0)
-  translate(75,350)
-
-  if (mpp == true){
-    vx = 400
-    vy = -300
-    circle(350, -260, 20);
+  p.preload = function() {
+    img = p.loadImage('dist/pictures/iu-kennlinie.png');
   }
 
-  if (paint == true) {
-    line(0,0,vx,vy);
+  p.setup = function() {
+    let cnv = p.createCanvas(600, 400);
+    cnv.parent('iukenn1');
   }
-}
 
-function mouseClicked() {
-  if (paint == false){
-    paint = true;
-  } else {
-    paint = false;
-  }
-}
+    p.draw = function() {
+      let myDegrees = p.map(p.mouseY, 0, p.height, 250, 400);
+      let v = p5.Vector.fromAngle(p.radians(myDegrees), 500);
+      let vx = v.x;
+      let vy = v.y;
 
-function keyPressed(){
-  if (keyCode === ENTER) {
-    if (mpp == false){
-      mpp = true;
-    } else {
-      mpp = false;
+      p.background(255)
+      p.image(img,0,0)
+      p.strokeWeight(3)
+      p.stroke(255,0,0)
+      p.translate(75,350)
+
+      if (mpp == true){
+        vx = 400
+        vy = -300
+        p.circle(350, -260, 20);
+      }
+
+      if (paint == true) {
+        p.line(0,0,vx,vy);
+      }
     }
-  } 
-}
+
+  p.mouseClicked = function() {
+    if (paint == false){
+      paint = true;
+    } else {
+      paint = false;
+    }
+  }
+
+  p.keyPressed = function(){
+    if (p.keyCode === ENTER) {
+      if (mpp == false){
+        mpp = true;
+      } else {
+        mpp = false;
+      }
+    } 
+  }
+};
+
+let p5sketch1 = new p5(sketch);
---
+++ docs/dist/mpptcalc.js
@@ -0,0 +1,102 @@
+let sketch2 = function(p){
+  let worldWidth = 640
+  let worldHeight = 400
+  let worldAspect = (worldHeight/worldWidth);
+  let hit = false;
+  let watts = 0;
+  let volts = 0;
+  let currents = 0;
+  let voltage = [,
+    50,
+    100,
+    150,
+    200,
+    250,
+    300,
+    350,
+    400,
+    410,
+    420];
+
+  let current = [300,
+    300,
+    290,
+    290,
+    280,
+    270,
+    250,
+    200,
+    100,
+    60];
+
+  p.setup = function() {
+    let canvas = p.createCanvas(p.windowWidth, p.windowWidth * worldAspect);
+    canvas.parent('mpptcalc1');
+    p.collideDebug(false);
+  }
+
+  p.draw = function() {
+    p.translate(0, p.height);
+    p.scale(p.width / worldWidth, -p.height / worldHeight);
+    p.background(255);
+    
+    let myDegrees = p.map(wMouseY(p.mouseY), 0, p.height, 0, 70);
+    let v = p5.Vector.fromAngle(p.radians(myDegrees), 500);
+    let vx = v.x;
+    let vy = v.y;
+    
+    let wp1 = p.createVector(voltage[0],current[0]);
+    let wp2 = p.createVector(voltage[0],current[0]);
+    
+    for (let i=0; i<=voltage.length; i++){
+      let p1 = p.createVector(voltage[i],current[i]);
+      let p2 = p.createVector(voltage[i+1],current[i+1]);
+      let p3 = p.createVector(0,0);
+      let p4 = p.createVector(vx, vy);
+      drawLines(wp1,wp2,p3,p4);
+      wp1 = p1;
+      wp2 = p2;
+    }
+  }
+
+  function drawLines(p1,p2,p3,p4){
+    hit = p.collideLineLineVector(p1,p2,p3,p4, true);
+    if(hit.x != false){
+      p.stroke(255,0,0);
+      p.fill(255,0,0);
+      p.circle(hit.x, hit.y, 20);
+      p.stroke(0,0,0);
+      p.strokeWeight(2);
+      p.fill(255,100,100,70);
+      p.stroke(255,100,100);
+      p.rect(0,0,hit.x,hit.y);
+      p.push()
+      p.scale(1,-1);
+      p.textSize(32);
+      volts = p.round(p.map(hit.x,0,500,0,30),1);
+      currents = p.round(p.map(hit.y,0,300,0,5),1);
+      watts = p.floor(volts*currents);
+      p.stroke(0,0,0,255);
+      p.text(watts + ' W', 450, -300);
+      p.text(volts + ' V', 450, -250);
+      p.text(currents + ' A', 450, -200);
+      p.pop();
+    }
+    p.strokeWeight(5);
+    p.stroke(0,0,255);
+    p.line(p1.x, p1.y, p2.x, p2.y);
+    p.stroke(240,200,0);
+    p.strokeWeight(3);
+    p.line(0, 0, p4.x, p4.y);
+  }
+
+  function wMouseX(pmouseX) {
+    return pmouseX * (worldWidth / p.width)
+  }
+
+  function wMouseY(pmouseY) {
+    return worldHeight - (pmouseY * (worldHeight / p.height))
+  }
+};
+
+let p5sketch2 = new p5(sketch2);
